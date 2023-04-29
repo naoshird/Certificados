edce3f446f2ff4f18a33f776c0f3f7bd13248019
Daniel Andres <95308596+naoshird@users.noreply.github.com>
2023-04-29 11:17:29 -0300

    Delete mysite/mysite directory

--- mysite/mysite/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def inicio(request):
-    return HttpResponse("¡Bienvenido a la página de inicio de Certificados de Mascotas!")
-
